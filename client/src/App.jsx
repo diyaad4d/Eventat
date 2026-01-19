2796fb2a32068c1f621e4a8e4ac43506b4cc4308
diyaa daifi <diyaadaifi@gmail.com>
2026-01-19 12:11:34 +0300

    structure of the FrontEnd

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './components/Landing/Landing.jsx';
+import Signup from './components/Signup/Signup.jsx';
+import Login from './components/Login/Login.jsx';
+import Home from './components/Home/Home.jsx';
+import Services from './components/Services/Services.jsx';
+
+
+function App() {
+    return (
+        <BrowserRouter>
+
+
+            <Routes>
+
+                <Route path="/" element={<Landing />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Home" element={<Home />} />
+                <Route path="/Services" element={<Services />} />
+
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
